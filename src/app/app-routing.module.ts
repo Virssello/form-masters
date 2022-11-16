f37499ffa5d664c3fd58185132ec9f340497e19a
Sebastian Jurczak <sebastian.jurczak@silevis.com>
2022-11-16 23:50:09 +0100

    Update app module

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,48 @@
+import { AppLayoutComponent } from '../layout/app.layout.component';
+import { NgModule } from '@angular/core';
+import { NotfoundComponent } from './pages/notfound/notfound.component';
+import { RouterModule } from '@angular/router';
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(
+      [
+        {
+          path: '',
+          component: AppLayoutComponent,
+          children: [
+            {
+              path: '',
+              loadChildren: () => import('./pages/dashboard/dashboard.module').then(({ DashboardModule }: any) => DashboardModule)
+            },
+            {
+              path: 'user',
+              loadChildren: () => import('./pages/user/user.module').then(({ UserModule }: any) => UserModule)
+            },
+            {
+              path: 'food',
+              loadChildren: () => import('./pages/food/food.module').then(({ FoodModule }: any) => FoodModule)
+            },
+            {
+              path: 'settings',
+              loadChildren: () => import('./pages/settings/settings.module').then(({ SettingsModule }: any) => SettingsModule)
+            },
+          ],
+        },
+        {
+          path: 'auth',
+          loadChildren: () => import('./pages/auth/auth.module').then(({ AuthModule }: any) => AuthModule),
+        },
+        { path: 'notfound', component: NotfoundComponent },
+        { path: '**', redirectTo: '/notfound' },
+      ],
+      {
+        scrollPositionRestoration: 'enabled',
+        anchorScrolling: 'enabled',
+        onSameUrlNavigation: 'reload',
+      }
+    ),
+  ],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,32 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { AppLayoutModule } from '../layout/app.layout.module';
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserModule } from '@angular/platform-browser';
 import { EffectsModule } from '@ngrx/effects';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { NotfoundComponent } from './pages/notfound/notfound.component';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { StoreModule } from '@ngrx/store';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NotfoundComponent
   ],
   imports: [
+    AppRoutingModule,
+    AppLayoutModule,
     BrowserModule,
     EffectsModule.forRoot([]),
-    StoreModule.forRoot({}, {})
+    StoreModule.forRoot({}, {}),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production })
   ],
-  providers: [],
+  providers: [{
+    provide: LocationStrategy, useClass: HashLocationStrategy
+  },],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
