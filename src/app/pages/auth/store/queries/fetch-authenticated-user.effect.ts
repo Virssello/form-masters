6d43389be208ac477bf0b01564aacac6aee84a5a
Sebastian Jurczak <sebastian.jurczak@silevis.com>
2022-11-22 22:29:24 +0100

    Update

--- src/app/pages/auth/store/queries/fetch-authenticated-user.effect.ts
+++ src/app/pages/auth/store/queries/fetch-authenticated-user.effect.ts
@@ -2,7 +2,7 @@ import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { AuthenticatedUserResponse } from '../response/authenticated-user.response';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { catchError, of, switchMap, tap } from 'rxjs';
+import { catchError, of, switchMap } from 'rxjs';
 import { fetchAuthenticatedUserAction, fetchAuthenticatedUserErrorAction, fetchAuthenticatedUserSuccessAction } from './fetch-authenticated-user.action';
 import { map } from 'rxjs/operators';
 
@@ -15,7 +15,6 @@ export class FetchAuthenticatedUserEffect {
     ofType(fetchAuthenticatedUserAction),
     switchMap(() => this.httpClient.get<AuthenticatedUserResponse>('api/auth/')
       .pipe(
-        tap(() => console.log('auth-user')),
         map((authenticatedUserResponse: AuthenticatedUserResponse) => fetchAuthenticatedUserSuccessAction({ authenticatedUserResponse: authenticatedUserResponse })),
         catchError((error: Error) => of(fetchAuthenticatedUserErrorAction({ error })))
       ))
